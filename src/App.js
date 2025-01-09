ff89c94027e65956f39504e8d27db89f42c8e569
Spagum <acrunchydesign@gmail.com>
2025-01-09 13:53:42 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,11 @@ function App() {
 
   return (
     <div>
-      <h1> seocnd test </h1>
+      <h1> bucket list. </h1>
+      <p> 100 things to do before i die or become a zombie. </p>
+      <p> completed: 2 </p>
+      <img src="zombieSticker1.png" />
+      <hr />
     </div>
   )
 
